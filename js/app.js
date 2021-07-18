3e8221fee6b5fbf8d7bc8ffbf2afad99bf965656
asraful <iasraful107@gmail.com>
2021-07-18 13:10:34 +0600

    hello

--- js/app.js
+++ js/app.js
@@ -4,5 +4,21 @@ $(document).ready(function(){
         dots: true,
         appendDots:'.slider-dots',
         dotsClass: 'dots'
-    })
+    });
+
+
+
+
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNAv = document.querySelector('.mobile-nav');
+
+    hamberger.addEventListener('.click' , function(){
+        mobileNAv.classList.add('open');
+
+    });
+    times.addEventListener('.click' , function(){
+        mobileNAv.classList.remove('open');
+    });
+
 });
